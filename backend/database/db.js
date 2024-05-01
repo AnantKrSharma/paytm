5db91bdcedc3263848a311bcf49b463b8b80d499
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-01 18:49:12 +0530

    Tailwind_Installation

---
+++ backend/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:12348765@cluster0.vjcfw4z.mongodb.net/Paytm')
+
+const userSchema = new mongoose.Schema(
+    {
+        username: String,
+        firstName: String,
+        lastName: String,
+        password: String
+    }
+)
+
+const Users = new mongoose.model('Users', userSchema)
