e19823c92d8a4a420c6187373bd9a04fe23cfc9b
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-02 19:12:46 +0530

    Routes

--- backend/database/db.js
+++ backend/database/db.js
@@ -13,6 +13,6 @@ const userSchema = new mongoose.Schema(
 
 const Users = mongoose.model('Users', userSchema)
 
-module.exports({
+module.exports = {
     Users
-})
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,28 @@
-const { db } = require('./database/db')
+const { Users } = require('./database/db');
 
-const express = require("express");
+const express = require('express')
 const app = express();
 app.use(express.json());
 
+app.get('/signup', async (req, res)=>{
+    await Users.create(
+        {
+            username: "Anant",
+            firstName: "A",
+            lastName: "K",
+            password: "69420"
+        }
+    ).then(()=>{
+        res.json({
+            msg: `User created`
+        })
+    }).catch(()=>{
+        res.json({
+            msg: "Error, fuck off"
+        })
+    });
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening to 3000.");
+})
