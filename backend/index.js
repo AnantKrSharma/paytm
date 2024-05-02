f09dc8de843a4581b30a4eaf11f94fa39249e0e7
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-02 19:58:35 +0530

    express.Router()

--- backend/index.js
+++ backend/index.js
@@ -4,25 +4,7 @@ const express = require('express')
 const app = express();
 app.use(express.json());
 
-app.get('/signup', async (req, res)=>{
-    
-    await Users.create(
-        {
-            username: "  AN  ",
-            firstName: "A",
-            lastName: "K",
-            password: "69420"
-        }
-    ).then(()=>{
-        res.json({
-            msg: `User created`
-        })
-    }).catch(()=>{
-        res.json({
-            msg: "Error, fuck off"
-        })
-    });
-})
+
 
 app.listen(3000, ()=>{
     console.log("Listening to 3000.");
