aca22e72fa28867e3ec6e8d9159efb18ad1ba4db
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-03 01:04:07 +0530

    Routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-const { Users } = require('./database/db');
-
 const express = require('express')
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+app.use(cors())
+const mainRouter = require('./routes/main')
 
-
+app.use('/api/v1', mainRouter)
 
 app.listen(3000, ()=>{
     console.log("Listening to 3000.");
--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,9 +1,7 @@
 const express = require('express')
-
 const router = express.Router();
+const userRoutes = require('./userRoutes')
 
-router.get('/signup', (req, res)=>{
-    res.send("YO");
-})
+router.use('/user', userRoutes);
 
 module.exports = router
