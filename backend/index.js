81352e87f290de7b7e5e9651ea3c2178dc8f0d5a
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-03 01:14:37 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const app = express();
+
 const mainRouter = require('./routes/main')
 
-const app = express();
 app.use(express.json());
 app.use(cors())
 
