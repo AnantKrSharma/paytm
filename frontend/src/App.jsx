773245b668e4d24492191b8ea4b18a36957da92c
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-10 00:50:59 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
+import { Suspense, lazy } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+// import { Signup } from './pages/Signup'
+// import { Signin } from './pages/Signin'
+// import { Dashboard } from './pages/Dashboard'
+// import { SendMoney } from './pages/SendMoney'
+import { Loading } from './components/Loading'
+const Signup = lazy( () => import('./pages/Signup')) 
+const Signin = lazy( () => import('./pages/Signin')) 
+const Dashboard = lazy( () => import('./pages/Dashboard')) 
+const SendMoney = lazy( () => import('./pages/SendMoney')) 
+
 
 function App() {
 
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+  return <div>
+    <BrowserRouter>
+
+        <Routes>
+
+            <Route path='/signup' element={<Suspense fallback={<Loading/>}> <Signup/> </Suspense>}></Route>
+            
+            <Route path='/signin' element={<Suspense fallback={<Loading/>}> <Signin/> </Suspense>}></Route> 
+            
+            <Route path='/dashboard' element={<Suspense fallback={<Loading/>}> <Dashboard/> </Suspense>}></Route>
+            
+            <Route path='/send' element={<Suspense fallback={<Loading/>}> <SendMoney/> </Suspense>}></Route>
+
+        </Routes>
+    
+    </BrowserRouter>
+  </div>
 }
 
 export default App
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,5 @@
+export function Loading(){
+    return <div className="flex justify-center font-serif font-light">
+        <p>Loading...</p>
+    </div>
+}
