ca2b20db07fb37621473bf5103c0f830f6ba9124
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-11 08:36:32 +0530

    new-changes

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -2,11 +2,11 @@ export function Input({label, type, placeholder}){
     
     return <div className="flex flex-col m-2 [&>*]:m-1">
         
-        <label htmlFor="inputBox" className="text-sm font-semibold">
+        <div className="text-sm font-semibold">
             {label} 
-        </label>
+        </div>
         
-        <input id='inputBox' type={type}  placeholder={placeholder} className="border shadow-sm p-2 rounded-md"/>
+        <input type={type}  placeholder={placeholder} className="border shadow-sm p-2 rounded-md"/>
     
     </div>
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,8 +3,15 @@ import {Subheading} from '../components/Subheading'
 import {Input} from '../components/Input'
 import {Button} from '../components/Button'
 import { BottomWarning } from '../components/BottomWarning'
+import { useState } from 'react'
 
 export default function Signup(){
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return <div className="flex justify-center items-center bg-neutral-500 h-screen">
         
             <div className='flex flex-col justify-center bg-neutral-100 border-0 rounded-md shadow-lg shadow-indigo-950'>
@@ -13,9 +20,21 @@ export default function Signup(){
 
                     <Subheading content={"Enter your details to create an account."}></Subheading>
                     
-                    <Input label={'First Name'} type={'text'} placeholder={'Anant'}></Input>
-                    <Input label={'Last Name'} type={'text'} placeholder={'Sharma'}></Input>
-                    <Input label={'Email'} type={'email'} placeholder={'example@mail.com'}></Input>
+                    <Input label={'First Name'} type={'text'} placeholder={'Anant'} onclick={(e)=>{
+                        setFirstName(e.target.value)
+                    }}></Input>
+                    
+                    <Input label={'Last Name'} type={'text'} placeholder={'Sharma'} onclick={(e)=>{
+                        setLastName(e.target.value)
+                    }}></Input>
+                    
+                    <Input label={'Email'} type={'email'} placeholder={'example@mail.com'} onclick={(e)=>{
+                        setUsername(e.target.value)
+                    }}></Input>
+
+                    <Input label={'Password'} type={'password'} placeholder={'example@mail.com'} onclick={(e)=>{
+                        setPassword(e.target.value)
+                    }}></Input>
                     
                     <div className="m-2">
                         <Button inner={'Sign-Up'} ></Button>
