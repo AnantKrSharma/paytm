3f3e50998443b785ed52e7f2ff0f091663b1f823
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-20 19:16:51 +0530

    vercel-deployment

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const app = express();
 const mainRouter = require('./routes/main')
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 
 app.use('/api/v1', mainRouter)
 
