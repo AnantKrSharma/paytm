88c01a77631d3756d7d990c7920db6eb30072e07
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-05-20 19:51:24 +0530

    main-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const Signup = lazy( () => import('./pages/Signup'))
 const Signin = lazy( () => import('./pages/Signin')) 
 const Dashboard = lazy( () => import('./pages/Dashboard')) 
 const SendMoney = lazy( () => import('./pages/SendMoney')) 
-
+const Main = lazy(()=> import('./pages/Main'))
 
 function App() {
 
@@ -17,6 +17,8 @@ function App() {
     <BrowserRouter>
 
         <Routes>
+
+            <Route path='/' element={<Suspense fallback={<Loading/>}> <Main/> </Suspense>}></Route>
 
             <Route path='/signup' element={<Suspense fallback={<Loading/>}> <Signup/> </Suspense>}></Route>
             
---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main(){
+    return <div className="flex justify-center items-center h-screen w-screen bg-blue-200">
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Paytm_logo.png" alt="PayTM" className="h-36 w-auto"/>
+    
+    </div>
+}
